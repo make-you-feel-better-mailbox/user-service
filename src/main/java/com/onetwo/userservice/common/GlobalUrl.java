909fdcacb72cd52ad5f709c5a57e6e3c518fc32d
Jeong Jeongil <jji042842@gmail.com>
2024-02-15 14:31:11 +0900

    Change root uri

--- src/main/java/com/onetwo/userservice/common/GlobalUrl.java
+++ src/main/java/com/onetwo/userservice/common/GlobalUrl.java
@@ -2,14 +2,14 @@ package com.onetwo.userservice.common;
 
 public class GlobalUrl {
 
-    public static final String ROOT_URI = "/";
+    public static final String ROOT_URI = "/user-service";
     public static final String UNDER_ROUTE = "/*";
     public static final String EVERY_UNDER_ROUTE = "/**";
 
-    public static final String USER_ROOT = "/users";
+    public static final String USER_ROOT = ROOT_URI + "/users";
     public static final String USER_LOGIN = USER_ROOT + "/login";
     public static final String USER_ID = USER_ROOT + "/id";
     public static final String USER_PW = USER_ROOT + "/pw";
-    public static final String TOKEN = "/token";
+    public static final String TOKEN = ROOT_URI + "/token";
     public static final String TOKEN_REFRESH = TOKEN + "/refresh";
 }
